520cd989d6f39889c219e83ec2439ce17196c5e5
20kdc <asdd2808@gmail.com>
2019-09-15 22:16:47 +0100

    No more faux-Impact-modules

--- modules/game/feature/custom-items/plug-in.js
+++
@@ -1,8 +0,0 @@
-ig.module(
-	'game.feature.custom-items.plug-in'
-)
-.requires(
-	'game.feature.custom-items.custom-items-model'
-)
-.defines(function () {});
-
--- modules/impact/feature/patches/plug-in.js
+++
@@ -1,9 +0,0 @@
-ig.module(
-	'impact.feature.patches.plug-in'
-)
-.requires(
-	'impact.feature.patches.patch',
-	'impact.feature.patches.patch-steps'
-)
-.defines(function () {});
-

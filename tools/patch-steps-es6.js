95b6de911bdf9c21f019137c8dcea77228b7676b
20kdc <asdd2808@gmail.com>
2019-09-19 20:12:30 +0100

    Update version number & test-case (NOT COMPLETE)
    NOT COMPLETE, I actually don't have the RAM to run anything like a sane testing setup
    Please tell browser developers to stop eating all the RAM just because they like their multi-process

--- tools/patch-steps-es6.js
+++ tools/patch-steps-es6.js
@@ -2,11 +2,11 @@
  * patch-steps-lib - Library for the Patch Steps spec.
  *
  * Written starting in 2019.
- * Version: 1.0.2
+ * Version: 1.1.0
  * (Ideally, this would comply with semver.)
  * Credits:
  *  Main code by 20kdc
- *  URL-style file paths, bughunting by ac2pic
+ *  URL-style file paths, FOR_IN, COPY, PASTE, error tracking, bughunting by ac2pic
  *
  * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
  * You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
